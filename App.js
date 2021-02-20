33df920c90e2a48f66892b49119eb9c6f90a33a0
Your Name <you@example.com>
2021-02-20 17:50:25 -0300

    Init project, title, subtitle, backgroundImage and initial  styles

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <View style={styles.carContainer}>
+
+        <ImageBackground 
+          source={require('./assets/images/ModelX.jpeg')}
+          style={styles.image}              
+        />
+
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subTitle}>Starting at $69,420</Text>
+        </View>
+      </View>
+
+
     </View>
+
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  carContainer: {
+    width: '100%',
+    height: '100%',
+    
+  },
+
+  titles: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+
+  subTitle: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
+  },
+  
 });
